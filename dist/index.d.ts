680bc68d27bead10869d9ed5154e7290a383b261
caique.moraes <ike.moraes93@gmail.com>
2023-03-28 09:54:26 -0300

    build: atualizado buil

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,6 +2,7 @@ type CallbackFilterFn<T> = (args: T) => boolean;
 type CallbackReduce<InitialType, T> = (previousValue: InitialType, currentValue: T) => InitialType;
 type CallbackMap<TInput, TOutput> = (arg: TInput) => TOutput;
 declare class ExtendedSet<T> extends Set<T> {
+    static of<T>(anArray: T[]): ExtendedSet<T>;
     filter(callbackFilterFn: CallbackFilterFn<T>): ExtendedSet<T>;
     reduce(callbackReduce: CallbackReduce<T, T>): T;
     reduce<InitialType>(callbackReduce: CallbackReduce<InitialType, T>, initialValue: InitialType): InitialType;
@@ -12,6 +13,8 @@ declare class ExtendedSet<T> extends Set<T> {
     intersection<TIntersectionSet extends T>(other: Set<TIntersectionSet>): ExtendedSet<T>;
     difference<TDifferenceSet extends T>(other: Set<TDifferenceSet>): ExtendedSet<T>;
     toArray(): T[];
+    get [Symbol.toStringTag](): string;
+    toString(): string;
     private _unionSets;
 }
 
