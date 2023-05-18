c8ffac7491eb2b1c3c082175c55ea68e8cadd098
caique.moraes <ike.moraes93@gmail.com>
2023-05-18 20:04:14 -0300

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,8 @@
 type CallbackFilter<Type> = (args: Type) => boolean;
 type CallbackReduce<InitialType, Type> = (previousValue: InitialType, currentValue: Type) => InitialType;
 type CallbackFind<Type> = (item: Type) => boolean;
+type CallbackEvery<Type> = CallbackFind<Type>;
+type CallbackSome<Type> = CallbackFind<Type>;
 type CallbackMap<TInput, TOutput> = (arg: TInput) => TOutput;
 declare class ExtendedSet<Type> extends Set<Type> {
     static of<Type>(anArray: Type[]): ExtendedSet<Type>;
@@ -9,6 +11,8 @@ declare class ExtendedSet<Type> extends Set<Type> {
     reduce<InitialType>(callbackReduce: CallbackReduce<InitialType, Type>, initialValue: InitialType): InitialType;
     map<TOutput>(callback: CallbackMap<Type, TOutput>): ExtendedSet<TOutput>;
     find(callback: CallbackFind<Type>): Type | null;
+    every(callback: CallbackEvery<Type>): boolean;
+    some(callback: CallbackSome<Type>): boolean;
     isSuperSetOf<TSuperSet extends Type>(other: Set<TSuperSet>): boolean;
     isSubSetOf(other: Set<Type>): boolean;
     union<TSubSet extends Type>(...others: Set<TSubSet>[]): ExtendedSet<Type>;
